7abe6c372bd5c4c8671183fc69f722f6053fb4ce
Sergio J Fernandez Marza <jeral.sergio@gmail.com>
2024-11-26 15:08:28 -0300

    Add faculti module completed

--- src/app/sections/admin/faculties/components/faculty-list/faculty-list.component.ts
+++ src/app/sections/admin/faculties/components/faculty-list/faculty-list.component.ts
@@ -1,16 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Faculty } from '../../models/faculty.model';
 
 @Component({
   selector: 'app-faculty-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './faculty-list.component.html',
   styleUrl: './faculty-list.component.scss'
 })
 export class FacultyListComponent {
   title: string = 'Facultad';
   path: string =  '/admin/faculties';
+  faculties: Faculty[] = [
+    {
+      id: 'kjlasdjf;asldkfl',
+      name: 'Tecnologia'
+    },
+    {
+      id: 'kjlasdjf;asldkfl',
+      name: 'Humanidades'
+    },
+    {
+      id: 'kjlasdjf;asldkfl',
+      name: 'Derecho'
+    }
+  ]
 
   constructor(
     private router: Router
@@ -18,5 +34,13 @@ export class FacultyListComponent {
 
   addFaculty(): void {
     this.router.navigate([this.path, 'create'])
+  }
+
+  openProfessorEdit(facultyId: string): void {
+
+  }
+
+  deleteProfessor(facultyId: string): void {
+
   }
 }
