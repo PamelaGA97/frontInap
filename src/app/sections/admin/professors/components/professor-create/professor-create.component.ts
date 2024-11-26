874b27b08d18d1eab8ec92c01074c56d60f91302
Sergio J Fernandez Marza <jeral.sergio@gmail.com>
2024-11-25 23:27:37 -0300

    Add professor create and list

--- src/app/sections/admin/professors/components/professor-create/professor-create.component.ts
+++ src/app/sections/admin/professors/components/professor-create/professor-create.component.ts
@@ -1,20 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ProfessorFormComponent } from '../../forms/professor-form/professor-form.component';
+import { Professor } from '../../models/professor.model';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-professor-create',
   standalone: true,
-  imports: [],
+  imports: [ProfessorFormComponent],
   templateUrl: './professor-create.component.html',
   styleUrl: './professor-create.component.scss'
 })
 export class ProfessorCreateComponent {
+  @ViewChild('professorForm') professorFormComponent!: ProfessorFormComponent;
 
+  constructor(
+    private location: Location
+  ) {}
 
   createProfessor(): void {
-
+    this.professorFormComponent.submit();
   }
 
   backToProfessorList(): void {
-    
+    this.location.back();
+  }
+
+  saveProfessor(value: Professor): void {
+    console.log('Aqui entra el guardar al docente')
   }
 }
--- src/app/sections/admin/professors/components/professor-list/professor-list.component.ts
+++ src/app/sections/admin/professors/components/professor-list/professor-list.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserRolEnum } from '../../../users/enums/user-rol.enum';
+import { Professor } from '../../models/professor.model';
 
 @Component({
   selector: 'app-professor-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './professor-list.component.html',
   styleUrl: './professor-list.component.scss'
 })
@@ -12,11 +15,46 @@ export class ProfessorListComponent {
   title: string = 'Docentes';
   path: string = '/admin/professors';
 
+  professors: Professor[] = [
+    {
+      id: 'caldsfjlasdfasldfl;',
+      course: 'Matematicas',
+      career: 'Electronica',
+      user: {
+        rol: UserRolEnum.TEACHER,
+        firstName: 'Andrea',
+        secondName: 'Lopez',
+        ci: '98127391',
+        cellphone: 77954876
+      }
+    },
+    {
+      id: 'ksajdfkajsdfkjl',
+      course: 'Matematicas',
+      career: 'Econimia',
+      user: {
+        rol: UserRolEnum.TEACHER,
+        firstName: 'Carlos',
+        secondName: 'Martinez',
+        ci: '983434391',
+        cellphone: 77634876
+      }
+    }
+  ];
+
   constructor(
     private router: Router
   ) {}
 
   addProfessor(): void {
     this.router.navigate([this.path, 'create'])
+  }
+
+  openProfessorEdit(professorsId: string) {
+    console.log(professorsId)
+  }
+
+  deleteProfessor(professorsId: string): void {
+    console.log(professorsId)
   }
 }
