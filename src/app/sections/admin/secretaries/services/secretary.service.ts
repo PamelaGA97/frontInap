94452efe8e9882479f7d76d2b74591d9dc14cc29
sergioJ92 <jeral.sergio@gmail.com>
2025-11-04 21:35:10 -0300

    Update secretary service

--- src/app/sections/admin/secretaries/services/secretary.service.ts
+++ src/app/sections/admin/secretaries/services/secretary.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Secretary } from '../models/secretary.model';
-import { Api } from '../../../../core/services/base-crud/decorators/api.decorator';
 import { environment } from '../../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
