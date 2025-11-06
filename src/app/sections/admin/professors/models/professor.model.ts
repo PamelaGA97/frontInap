adf607f07651ebd27341833fd8ee4f5f8d92453b
sergioJ92 <jeral.sergio@gmail.com>
2025-11-05 22:42:03 -0300

    Update professor model

--- src/app/sections/admin/professors/models/professor.model.ts
+++ src/app/sections/admin/professors/models/professor.model.ts
@@ -2,12 +2,20 @@ import { BaseModel } from "../../../../shared/models/baseModel.model";
 import { ClassSchedule } from "../../class-schedule/models/class-schedule.model";
 import { Course } from "../../courses/model/course.model";
 import { Faculty } from "../../faculties/models/faculty.model";
-import { User } from "../../users/model/user.model";
+import { UserRolEnum } from "../../users/enums/user-rol.enum";
 
 export interface Professor extends BaseModel{
     id: string,
-    course?: Course,
-    faculty?: Faculty,
-    user: User,
-    classSchedules?: ClassSchedule[]
+    firstName?: string;
+    secondName?: string;
+    ci?: string;
+    phone?: string;
+    rol?: UserRolEnum;
+    email?: string;
+    password?: string;
+    isAvaible?: boolean;
+    salary?: number
+    // course?: Course,
+    // faculty?: Faculty,
+    // classSchedules?: ClassSchedule[]
 }
