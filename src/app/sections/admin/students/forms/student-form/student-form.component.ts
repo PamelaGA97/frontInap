81f42932d8f2510bdeef9fd2e3ec21f2793d6510
sergioJ92 <jeral.sergio@gmail.com>
2025-12-03 23:29:30 -0300

    Add faculti and degree into stident form component

--- src/app/sections/admin/students/forms/student-form/student-form.component.ts
+++ src/app/sections/admin/students/forms/student-form/student-form.component.ts
@@ -1,195 +1,217 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
+import { firstValueFrom, Subject } from 'rxjs';
 import { UserRolEnum } from '../../../users/enums/user-rol.enum';
 import { BlockInvalidNumberKeysDirective } from '../../../../../core/directives/block-invalid-number-keys.directive';
 import { FormStatus } from '../../../../../shared/enums/form-status.enum';
 import { ValidatioErrorMessage } from '../../../../../core/validation-error-message';
 import { FacultyService } from '../../../faculties/services/facuties.service';
 import { Faculty } from '../../../faculties/models/faculty.model';
-import { Career } from '../../../careers/models/career.model';
-import { CommonModule } from '@angular/common';
-import { firstValueFrom } from 'rxjs';
 import { matchValidator } from '../../../../../shared/validations/validation-password';
 import { User } from '../../../users/model/user.model';
 import { generateYearList, parseDateYearToNumber } from '../../../../../core/utils/date.utils';
-
+import { GenericStore } from '../../../../../shared/store/generic-crud.store';
+import { ToastService } from '../../../../../shared/services/toast.service';
+import { FacultySelectComponent } from '../../../../../shared/components/faculty-select/faculty-select.component';
+import { Degree } from '../../../degrees/models/degree.model';
+import { PaginationResponse } from '../../../../../shared/models/pagination-response.model';
 @Component({
     selector: 'app-student-form',
     standalone: true,
-    imports: [CommonModule, ReactiveFormsModule, BlockInvalidNumberKeysDirective],
+    imports: [
+		CommonModule,
+		ReactiveFormsModule,
+		BlockInvalidNumberKeysDirective,
+		FormsModule,
+		FacultySelectComponent,
+    ],
     templateUrl: './student-form.component.html',
     styleUrl: './student-form.component.scss'
 })
 export class StudentFormComponent {
-  @Input() studentData?: User;
-  @Input() isPreview: boolean = false;
-  @Output() submitFormEvent = new EventEmitter<User>();
-  studentForm!: FormGroup;
-  formStatusEnum = FormStatus;
-  validationErrorMessage = ValidatioErrorMessage;
-  minYear: string[] = [];
-  faculties: Faculty[] = [];
-  careerList: Career[] = [];
-  years: number[] = [];
-  initialYear: number = 2000;
+	@Input() studentData?: User;
+	@Input() isPreview: boolean = false;
+	@Output() submitFormEvent = new EventEmitter<User>();
+	studentForm!: FormGroup;
+	formStatusEnum = FormStatus;
+	validationErrorMessage = ValidatioErrorMessage;
+	minYear: string[] = [];
+	degreeList: Degree[] = [];
+	years: number[] = [];
+	initialYear: number = 2000;
 
-  constructor(
-    private _formBuilder: FormBuilder,
-    private facultyService: FacultyService,
-  ) {
-  }
-  
-  ngOnInit(): void {
-    this.initialize();
-    this.loadFormValues();
-    this.addStudentDataToForm();
-  }
-  
-  private initialize(): void {
-    this.initializeForm();
-  }
+	private destroy$ = new Subject<void>();
+	facultyOptions$ = new Subject<string>();
+	faculties: Faculty[] = [];
+	selectedFaculty?: Faculty;
 
-  private async loadFormValues(): Promise<void> {
-    this.years = generateYearList(this.initialYear);
-    await this.loadFaculties();
-  }
+	constructor(
+		private _formBuilder: FormBuilder,
+		private _toastService: ToastService,
+		private _facultyService: FacultyService,
+		public store: GenericStore<Faculty>,
+	) {}
 
-  private initializeForm(): void {
-    const isEditMode = !!this.studentData;
-    this.studentForm = this._formBuilder.group({
-      firstName: ['', [Validators.required]],
-      secondName: ['', [Validators.required]],
-      ci: ['', [Validators.required]],
-      phone: ['', [Validators.required]],
-      rol: [UserRolEnum.STUDENT, [Validators.required]],
-      isAvaible: [true, [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', isEditMode ? [] : [Validators.required]],
-      confirmPassword: ['', isEditMode ? [] : [Validators.required]],
-      highschool: ['', [Validators.required]],
-      graduationYear: ['', [Validators.required]],
+	ngOnInit(): void {
+		this.initialize();
+		this.loadFormValues();
+		this.addStudentDataToForm();
+	}
 
-      // faculty: [null, [Validators.required]],
-      // career: ['', [Validators.required]],
-    },{ validators: matchValidator('password', 'confirmPassword') });
-  }
+	private initialize(): void {
+		this.initializeForm();
+	}
 
-  private async loadFaculties(): Promise<void> {
-    await firstValueFrom(this.facultyService.getAll())
-      .then((response) => {
-        this.faculties = response;
-      }).catch((error) => {
-        console.error(error);
-      });
-  }
+	private async loadFormValues(): Promise<void> {
+		this.years = generateYearList(this.initialYear);
+		this.facultyOptions$.next("");
+	}
 
-  // private generateYears(): void {
-  //   const currentYear = new Date().getFullYear();
-  //   const startYear = 2000;
-  //   this.years = Array.from(
-  //     { length: currentYear - startYear + 1 },
-  //     (_, i) => new Date(`${currentYear - i}-01-01T03:00:00.000Z`).getFullYear()
-  //   );
-  // }
+	searchFaculty(event: { term: string, items: any }) {
+		this.facultyOptions$.next(event.term)
+	}
 
-  private async addStudentDataToForm(): Promise<void> {
-    if(this.studentData){
-      this.studentForm.patchValue(this.studentData);
-      await this.addGraduationYearToForm();
-      // this.addFacultyToForm();
-      // this.addCareerToForm();
-    }
-  }
+	private initializeForm(): void {
+		const isEditMode = !!this.studentData;
+		this.studentForm = this._formBuilder.group({
+			firstName: ['', [Validators.required]],
+			secondName: ['', [Validators.required]],
+			ci: ['', [Validators.required]],
+			phone: ['', [Validators.required]],
+			rol: [UserRolEnum.STUDENT, [Validators.required]],
+			isAvaible: [true, [Validators.required]],
+			email: ['', [Validators.required, Validators.email]],
+			password: ['', isEditMode ? [] : [Validators.required]],
+			confirmPassword: ['', isEditMode ? [] : [Validators.required]],
+			highschool: ['', [Validators.required]],
+			graduationYear: ['', [Validators.required]],
+			faculty: [null, [Validators.required]],
+			degree: ['', [Validators.required]],
+		},{
+			validators: matchValidator('password', 'confirmPassword')
+		});
+	}
 
-  private async addGraduationYearToForm(): Promise<void> {
-    const generateYears = await parseDateYearToNumber(this.studentData?.graduationYear);
-    this.studentForm.controls['graduationYear'].setValue(generateYears);
-  }
+	// private generateYears(): void {
+	//   const currentYear = new Date().getFullYear();
+	//   const startYear = 2000;
+	//   this.years = Array.from(
+	//     { length: currentYear - startYear + 1 },
+	//     (_, i) => new Date(`${currentYear - i}-01-01T03:00:00.000Z`).getFullYear()
+	//   );
+	// }
 
-  parceDateToYear(date?: string): number {
-    const year = parseDateYearToNumber(date);
-    return year ? year : 0 ;
-  }
+	private async addStudentDataToForm(): Promise<void> {
+		if(this.studentData){
+		this.studentForm.patchValue(this.studentData);
+		await this.addGraduationYearToForm();
+		// this.addFacultyToForm();
+		// this.addCareerToForm();
+		}
+	}
 
-  // private addFacultyToForm(): void {
-  //   const facultyFounded = this.faculties.find((faculty)=>(faculty.id === this.studentData?.faculty?.id));
-  //   this.studentForm.controls['faculty'].setValue(facultyFounded);
-  //   this.loadCareers();
-  // }
+	private async addGraduationYearToForm(): Promise<void> {
+		const generateYears = await parseDateYearToNumber(this.studentData?.graduationYear);
+		this.studentForm.controls['graduationYear'].setValue(generateYears);
+	}
 
-  // private addCareerToForm(): void {
-  //   const careerFounded = this.careerList.find((career)=>(career.id === this.studentData?.career?.id));
-  //   this.studentForm.controls['career'].setValue(careerFounded);
-  // }
+	parceDateToYear(date?: string): number {
+		const year = parseDateYearToNumber(date);
+		return year ? year : 0 ;
+	}
 
-  // private parseDateTuNumber(): number {
-  //   return this.studentData?.graduationYear ? new Date(this.studentData.graduationYear).getFullYear() : 2024;
-  // }
+	// private addFacultyToForm(): void {
+	//   const facultyFounded = this.faculties.find((faculty)=>(faculty.id === this.studentData?.faculty?.id));
+	//   this.studentForm.controls['faculty'].setValue(facultyFounded);
+	//   this.loadCareers();
+	// }
 
-  loadCareers(): void {
-    const faculty = this.studentForm.value.faculty;
-    this.careerList = faculty.careers;
-    // this.career.setValue('')
-  }
+	// private addCareerToForm(): void {
+	//   const careerFounded = this.degreeList.find((career)=>(career.id === this.studentData?.career?.id));
+	//   this.studentForm.controls['career'].setValue(careerFounded);
+	// }
 
-  submit(): void {
-    this.studentForm.markAllAsTouched();
-    if (this.studentForm.valid) {
-      this.setGraduationDateFormat()
-      this.submitFormEvent.emit(this.studentForm.value);
-    }
-  }
+	// private parseDateTuNumber(): number {
+	//   return this.studentData?.graduationYear ? new Date(this.studentData.graduationYear).getFullYear() : 2024;
+	// }
 
-  private setGraduationDateFormat(): void {
-    const dateString = new Date(`${this.studentForm.value.graduationYear}-01-01T00:00:00.000Z`).toISOString();
-    this.studentForm.controls['graduationYear'].setValue(dateString);
-  }
+	async loadDegrees($facultyId: string): Promise<void> {
+		this.degree.setValue('');
+		await firstValueFrom(this._facultyService.getDegrees($facultyId))
+		.then(
+			(response: PaginationResponse<Degree>) => {
+			this.degreeList = response.data;
+			})
+		.catch((error: Partial<HttpErrorResponse>) => {
+			this._toastService.showHttpError(error.error);
+		});
+	}
 
-  get firstName() {
-    return this.studentForm?.controls['firstName'];
-  }
+	submit(): void {
+		this.studentForm.markAllAsTouched();
+		if (this.studentForm.valid) {
+			this.setGraduationDateFormat()
+			this.submitFormEvent.emit(this.studentForm.value);
+		}
+	}
+
+	private setGraduationDateFormat(): void {
+		const dateString = new Date(`${this.studentForm.value.graduationYear}-01-01T00:00:00.000Z`).toISOString();
+		this.studentForm.controls['graduationYear'].setValue(dateString);
+	}
+
+	get firstName() {
+		return this.studentForm?.controls['firstName'];
+	}
 
 	get secondName() {
 		return this.studentForm?.controls['secondName'];
 	}
 
-  get ci() {
+	get ci() {
 		return this.studentForm?.controls['ci'];
 	}
 
-  get phone() {
+	get phone() {
 		return this.studentForm?.controls['phone'];
 	}
 
-  get email() {
-    return this.studentForm?.controls['email'];
-  }
+	get email() {
+		return this.studentForm?.controls['email'];
+	}
 
-  get password() {
-    return this.studentForm?.controls['password'];
-  }
+	get password() {
+		return this.studentForm?.controls['password'];
+	}
 
-  get confirmPassword() {
-    return this.studentForm.controls['confirmPassword'];
-  }
+	get confirmPassword() {
+		return this.studentForm.controls['confirmPassword'];
+	}
 
-  get highschool() {
-    return this.studentForm.controls['highschool'];
-  }
+	get highschool() {
+		return this.studentForm.controls['highschool'];
+	}
 
-  get graduationYear () {
-    return this.studentForm.controls['graduationYear'];
-  }
+	get graduationYear () {
+		return this.studentForm.controls['graduationYear'];
+	}
 
-  get isAvaible() {
-    return this.studentForm.controls['isAvaible'];
-  }
+	get isAvaible() {
+		return this.studentForm.controls['isAvaible'];
+	}
 
-  // get career() {
-  //   return this.studentForm.controls['career'];
-  // }
+	get degree() {
+		return this.studentForm.controls['degree'];
+	}
 
-  // get faculty() {
-  //   return this.studentForm.controls['faculty']
-  // }
+	get faculty() {
+		return this.studentForm.controls['faculty']
+	}
+
+	ngOnDestroy(): void {
+		this.destroy$.next();
+		this.destroy$.complete();
+	}
 }
